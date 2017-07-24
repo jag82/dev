661678c7e9592f805c85e90c778c92b98dc8306f
Jag <jag.chadha@gmail.com>
2017-07-24 14:22:26 +0200

    updating init script

--- _scripts/_init.sh
+++ _scripts/_init.sh
@@ -4,7 +4,7 @@
 rm -rf .git/hooks
 
 #simlink our githooks dir, which is version controlled, to .git/hooks so they're run
-ln -sf _githooks .git/hooks
+ln -sf ../../_githooks .git/hooks
 
 #make sure we have permission to run our githooks
 chmod +x _githooks/*
